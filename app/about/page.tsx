2db86a3e08e32deb6e5c10646e0b892d48e2a209
Bryce Lucas <brycelucas@oakland.edu>
2025-11-05 12:39:58 -0500

    adding very minimal text to about page to fix aws upload

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <main className="p-6">
+      <h1>About EventHub</h1>
+      <p>Team overview, goals, and tech stack.</p>
+    </main>
+  );
+}
